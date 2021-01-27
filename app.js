1446ca4ac9666a00648482d2e6fdef4996ad5769
mohammad mahmoud mohammad hassouneh <mohammad.hs91@hotmail.com>
2021-01-27 14:25:44 +0200

    update

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log('study hard');
+console.log(7-2);
+
+var userName = 'mohammad';
+      console.log(userName);
+
+      var x = 5;
+      console.log(x>=5);
+      
+      alert('fight');
+
+      var levelNow= prompt('what level filight'); 
+      var message = '';
+
+      console.log(levelNow);
+
+      if (levelNow > 35 ){message = 'very high'; }
+
+      else if ( levelNow > 18 ){ message = 'in the cloud'}
+else if (levelNow >= 5){ message= 'very nice'}
+
+  else { message = 'nice flight'}
+
+  document.write('<h2>' + message +'</h2>');
+  
+
+
