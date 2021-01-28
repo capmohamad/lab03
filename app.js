9ad0b2942a50f31afcfd471bae2c235dd8bbaa4d
mohammad mahmoud mohammad hassouneh <mohammad.hs91@hotmail.com>
2021-01-28 15:53:13 +0200

    newnote

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-console.log('study hard');
-console.log(7-2);
+// console.log('study hard');
+// console.log(7 - 2);
 
-var userName = 'mohammad';
-      console.log(userName);
+// var userName = 'mohammad';
+// console.log(userName);
 
-      var x = 5;
-      console.log(x>=5);
-      
-      alert('fight');
+// var x = 5;
+// console.log(x >= 5 );
 
-      var levelNow= prompt('what level filight'); 
-      var message = '';
+// alert('fight');
 
-      console.log(levelNow);
+var levelNow = prompt('what level filght');
+var message = '';
 
-      if (levelNow > 35 ){message = 'very high'; }
+console.log(levelNow);
 
-      else if ( levelNow > 18 ){ message = 'in the cloud'}
-else if (levelNow >= 5){ message= 'very nice'}
+if (levelNow <= 35 && levelNow >= 18) { message = 'very high'; }
 
-  else { message = 'nice flight'}
+else if (levelNow < 18 && levelNow > 5) { message = 'in the cloud' }
+else if (levelNow <= 5) { message = 'to law' }
 
-  document.write('<h2>' + message +'</h2>');
-  
+else { message = 'bye bye' }
+
+document.write('<h2>' + message + '</h2>');
+
+var order = prompt('what colore you want blue or black?');
+
+//order = red
+
+while (order !== 'blue' && order !== 'black') {
+    order = prompt('what colore you want blue or black?');
+}
+var colore = '';
+
+var imageNum = prompt('how many images do you want to print?');
+
+for(var i = 1 ; i <= imageNum; i++){
+    if (order === 'blue') { colore = '<img src="images/blue.jpg"/>' }
+    else if (order === 'black') { colore = '<img src="images/black.jpg" />' };
+    document.write(colore);
+}
+
+
+
+
+
+
+
 
 
